c8e3f9c46fafa62f194d93fb28fb5193d3476bd9
Rodrigo <cardosodesarodrigo1@gmail.com>
2024-12-25 20:30:33 -0300

    Criação da tela de introdução

---
+++ app/src/main/java/com/rodrigosa/shoesshop/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.rodrigosa.shoesshop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rodrigosa.shoesshop.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+
+    }
+}
--- app/src/main/java/com/rodrigosa/shoesshop/MainActivity.kt
+++ app/src/main/java/com/rodrigosa/shoesshop/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.rodrigosa.shoesshop
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_constraint)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
